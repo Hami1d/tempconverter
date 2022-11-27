e1a3bd441f730ad3e9272ec7fb77b571672b1a56
Hami1d <61083938+Hami1d@users.noreply.github.com>
2022-11-27 18:37:50 +0100

    Update tempconverter.py

--- tempconverter.py
+++ tempconverter.py
@@ -7,21 +7,21 @@ def main():
     if choose == 1:
         print(f'{temp} °C\n')
         CtoF = (temp * 9/5) + 32
-        CtoK = temp + 273,15
+        CtoK = temp + 273.15
         print(f'Celsius to Fahrenheit: {CtoF}°F \nCelsius to Kelvin: {CtoK}°K\n')
         main()
     
     elif choose == 2:
         print(f'{temp} °F\n')
         FtoC = (temp - 32) * 5/9
-        FtoK = (temp - 32) * 5/9 + 273,15
+        FtoK = (temp - 32) * 5/9 + 273.15
         print(f'Fahrenheit to Celsius: {FtoC}°C \nFahrenheit ro Kelvin: {FtoK}°K\n')
         main()
     
     elif choose == 3:
         print(f'{temp} °K\n')
         KtoF = (temp - 273,15) * 9/5 + 32
-        KtoC = temp - 273,15
+        KtoC = temp - 273.15
         print(f'Kelvin to Fahrenheit: {KtoF}°F \nKelvin ro Celsius: {KtoC}°C\n')
         main()
     
